e989526f8f1936bd1f60ddae6fd93d6f33eab96c
Renann Santos <69852026+Renann1@users.noreply.github.com>
2021-04-07 19:27:17 -0300

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,106 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Controller extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Controller</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Controller at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         //quando for clicado no link da index, vai vir para cá
+         //verificar a variável "op"
+         String opcao = request.getParameter("op");
+         
+         switch (opcao){
+             case "novo":
+                 // aqui vamos redirecionar para a view correta (cliente)
+                 // criação de um atributo que será utilizado entre as páginas JSP
+                 request.setAttribute("msg", "Você clicou em Novo");
+                 //despachar
+                 request.getRequestDispatcher("view_mensagem.jsp").forward(request, response);
+                 
+                 
+                 break;
+             
+             case "pesquisar":
+                 break;
+             
+             case "listar":
+                 switch(opcao){
+                     case "listar":
+                         
+                         request.setAttribute("form", "Formulário faculdade");
+                         request.getRequestDispatcher("view_novo.jsp").forward(request, response);
+                        
+                 }
+                         
+                 break;
+         }
+         
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
